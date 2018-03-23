915bb59506e423e07ffd1641ae999e64a48da3ae
rgreen88 <rynelgreen@gmail.com>
2018-03-23 11:46:11 -0400

    First commit with SensorHelper dependency

---
+++ app/src/main/java/com/example/android/dummy/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.dummy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.sensorhelper.SensorHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    SensorHelper sensorHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  sensorHelper;
+    }
+}
