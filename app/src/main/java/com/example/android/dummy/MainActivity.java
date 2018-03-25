e5ca9ccf4fffad1ef1ada3ad7012f0f734c15dc7
rgreen88 <rynelgreen@gmail.com>
2018-03-25 16:22:20 -0400

    added logd to event which passes null. SensorCallback interface still inaccessible.

--- app/src/main/java/com/example/android/dummy/MainActivity.java
+++ app/src/main/java/com/example/android/dummy/MainActivity.java
@@ -1,19 +1,77 @@
 package com.example.android.dummy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.example.android.sensorhelper.SensorHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorCallback{
 
+    private static final String TAG = "value onSensorChange";
     SensorHelper sensorHelper;
+    SensorEvent event;
+    private Display mDisplay;
+    private WindowManager mWindowManager;
+    private float mSensorX;
+    private float mSensorY;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView1);
+        Log.d(TAG, "onCreate: " + String.valueOf(event)); //null value with non-working interface in lib
+        sensorHelper.onSensorChanged(event);
 
-      //  sensorHelper;
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+                return;
+        }
+        Log.d(TAG, "onSensorChanged: ");
+
+
+        switch (mDisplay.getRotation()) {
+            case Surface.ROTATION_0:
+                mSensorX = event.values[0];
+                mSensorY = event.values[1];
+                textView.setText(String.valueOf(mSensorX));
+                Log.d(TAG, "onSensorChanged: "  + String.valueOf(mSensorX)) ;
+
+                break;
+            case Surface.ROTATION_90:
+                mSensorX = -event.values[1];
+                mSensorY = event.values[0];
+                textView.setText(String.valueOf(mSensorX));
+                Log.d(TAG, "onSensorChanged: "  + String.valueOf(mSensorX));
+
+                break;
+            case Surface.ROTATION_180:
+                mSensorX = -event.values[0];
+                mSensorY = -event.values[1];
+                textView.setText(String.valueOf(mSensorX));
+                Log.d(TAG, "onSensorChanged: "  + String.valueOf(mSensorX));
+
+                break;
+            case Surface.ROTATION_270:
+                mSensorX = event.values[1];
+                mSensorY = -event.values[0];
+                textView.setText(String.valueOf(mSensorX));
+                Log.d(TAG, "onSensorChanged: " + String.valueOf(mSensorX));
+                break;
+        }
     }
 }
